5fc35c36a5da6e483a4bb07edff3636709daaf21
VaibhavBhawsar <127779539+VaibhavBhawsar@users.noreply.github.com>
2025-02-11 19:06:28 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 # AWS provider and backend
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIASO4GC2DYR5FOSBHU"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
